86821cade536f0609cf0114645f04869f713041a
Fabio Lisboa <fabiodejesuslisboa@gmail.com>
2024-11-12 19:47:21 -0300

    My first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Jornada de dados")
+
+#Variáveis
+idade = 29
+Nome = "Fabio Lisboa"
+
+#Print
+print(idade)
+
+#Soma
+new_age = idade+1
+print(new_age)
+
+#Listas
+lista = [1,2,3,4,5]
+
+#Métodos
+reverse_list = lista.reverse()
+
